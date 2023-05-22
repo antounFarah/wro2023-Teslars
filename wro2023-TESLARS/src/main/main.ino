71cce34a9e9d34e5a9eacb8045bf8a5409bb45a9
antounFarah <tmshmwsht@gmail.com>
2023-05-22 13:36:38 +0300

    make imu class with little edits.

--- wro2023-TESLARS/src/main/main.ino
+++ wro2023-TESLARS/src/main/main.ino
@@ -1,10 +1,4 @@
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
+#include <myimu.h>
 #include <Servo.h>
 
 Servo servo1;
@@ -12,9 +6,15 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 Servo steering;
+myimu IMU;
 
-MPU6050 mpu;
 
+// used arduino mega pins
+/*
+4, 5, 6, 7, 18, 19: ultrasonic
+8, 9, 10, 11 : ultrasonic servos
+
+*/
 const int trigpin[] = {4, 5, 6, 7};
 const int echopin[] = {2, 3, 18, 19};
 
@@ -27,72 +27,8 @@ volatile long duration[4];
 volatile long timer[4];
 volatile double distance[4];
 
-#define OUTPUT_READABLE_YAWPITCHROLL
-#define INTERRUPT_PIN 2
-bool blinkState = false;
-
-bool dmpReady = false;
-uint8_t mpuIntStatus;
-uint8_t devStatus;
-uint16_t packetSize;
-uint16_t fifoCount;
-uint8_t fifoBuffer[64];
-Quaternion q;
-VectorInt16 aa;
-VectorInt16 aaReal;
-VectorInt16 aaWorld;
-VectorFloat gravity;
-
-float euler[3];
-float ypr[3];
-uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
-
-volatile bool mpuInterrupt = false;
-
 void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    Wire.begin();
-    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-    Fastwire::setup(400, true);
-#endif
-    Serial.begin(115200);
-    while (!Serial)
-        ;
-    mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
-
-    // Serial.println(F("Testing device connections..."));
-    //  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-    devStatus = mpu.dmpInitialize();
-    // offset
-    mpu.setXGyroOffset(41);
-    mpu.setYGyroOffset(20);
-    mpu.setZGyroOffset(0);
-    mpu.setZAccelOffset(-10);
-
-    if (devStatus == 0)
-    {
-        // Calibration Time: generate offsets and calibrate our MPU6050
-        mpu.CalibrateAccel(6);
-        mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
-        // turn on the DMP, now that it's ready
-        // Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-        // Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    }
-    else
-    {
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
+    IMU.imu_init();
     for (i = 0; i < 4; i++)
     {
         pinMode(trigpin[i], OUTPUT);
@@ -110,31 +46,14 @@ void setup() {
 }
 void loop()
 {
-    //steering.write(pid(distance[2], 40, 1, 0.0002, 0.5));
-    if (!dmpReady)
-        return;
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
-    {
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        Serial.print("\t");
-        yaw = ypr[0] * 180 / M_PI;
-        
-        servo1.write(yaw);
-        servo2.write(yaw);
-        servo3.write(yaw);
-        servo4.write(yaw); 
+    yaw = IMU.readyaw();
 
-        Serial.println(yaw);
-    }
+    servo1.write(yaw);
+    servo2.write(yaw);
+    servo3.write(yaw);
+    servo4.write(yaw);
+
 }
-
-void dmpDataReady()
-{
-    mpuInterrupt = true;
-}
-
 void send_trig(int x)
 {
     measured[x] = 1;
